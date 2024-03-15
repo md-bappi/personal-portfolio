7d1722738b181361f382a8d4fb16d634bfdcee2a
md-bappi <mdabuanasbappy555@gmail.com>
2024-03-15 20:14:18 +0600

    hero section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app container mx-auto">
+      <div className="noise"></div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
